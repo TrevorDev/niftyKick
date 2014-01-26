38d049274f04b9f03d1dd837d5502c10226fb3a3
Trevor <trevorjbaron@gmail.com>
2014-01-25 22:09:49 -0500

    setup db

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 var render = require('./lib/render');
+var config = require('./lib/config');
+var db = require('./lib/database');
 var logger = require('koa-logger');
 var router = require('koa-router');
 var serve = require('koa-static');
+var session = require('koa-session');
 var views = require('co-views');
 var parse = require('co-body');
 var koa = require('koa');
 var swig = require('swig');
 var app = koa();
 
+db.getConn();
 //REMOVE IN PRODUCTION??
 swig.setDefaults({ cache: false })
 
 //ROUTES
+app.keys = [config.sessionSecret];
+app.use(session());
+
 app.use(router(app));
 app.get('/', index);
 app.get('/public/*', serve('.'));
