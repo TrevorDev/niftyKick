bebe07d34289689b8780e43155a15b6acbce0870
Trevor <trevorjbaron@gmail.com>
2014-02-17 20:28:01 -0500

    serve files to users who have purchased them

--- controllers/file.js
+++ controllers/file.js
@@ -3,6 +3,7 @@ var config = require('../lib/config');
 var fileSys = require('../lib/fileSys');
 var file = require('./../models/file');
 var project = require('./../models/project');
+var user = require('./../models/user');
 var path = require('path');
 var sessionHelper = require('./../lib/sessionHelper');
 var fs = require('co-fs');
@@ -13,10 +14,20 @@ var upload = require('./../lib/upload');
 
 exports.download = function * () {
   var d = yield file.getDownload(this.params.id);
+  var accessAllowed = false;
   if(d.price==0){
+  	accessAllowed = true;
+  }else{
+  	var owned = yield user.getPurchase(sessionHelper.getUserID(this.session), d.project_id);
+  	if(owned){
+  		accessAllowed = true;
+    }
+  }
+
+  if(accessAllowed){
   	var filePath = path.join(project.getFilesFolder(d.project_id, d.user_id), d.name)
-    yield send(this, filePath)
-    this.set('Content-Disposition', "attachment; filename="+d.name);
-    this.set('content-type', "application/octet-stream");
+	  yield send(this, filePath)
+	  this.set('Content-Disposition', "attachment; filename="+d.name);
+	  this.set('content-type', "application/octet-stream");
   }
 }
